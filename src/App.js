ef4148ecbded279b371ed54f29d8611fb005fd47
pteacher <subfear@mail.ru>
2020-12-04 10:37:53 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Bit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      val: this.props.val,
+    }
+  }
+
+  render() {
+    return (
+      <button onClick={this.props.onClick}>{this.state.val}</button>
+      )
+  }
 }
 
-export default App;
+function getrand(a, b) {
+  return Math.floor(Math.random() * (a + b - 1)) + a;
+}
+
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bits: new Array(8).fill(0),
+      decimal: getrand(1, 10),
+    }
+  }
+
+  renderBits(i) {
+    return (<Bit 
+      val={this.state.bits[i]}
+      onClick={() => this.handleClick(i)}
+      />)
+  }
+
+  handleClick(i) {
+    this.setState({
+      val: (this.state.bits[i] === '0') ? '1' : '0',
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Binary game!</div>
+        <div>{ this.state.decimal }</div>
+        {new Array(8).fill(0).map ((n) => {return this.renderBits(n)})}
+      </div>
+      )
+  }
+}
+
+export default Game;
